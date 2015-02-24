7ec85c6a19a7e4ac33a8c7e26e3e80a241c70a7e
Nick Weedon n2798271 <nick@weedon.org.au>
2015-02-24 23:07:16 +1000

    Added test target

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,14 +23,40 @@ module.exports = function(grunt) {
 		},
 		jshint: {
 		    all: ['src/**/*.js']
-		}
+		},
+		"bower-install-simple": {
+	        options: {
+	            color: true,
+	        },
+	        "prod": {
+	            options: {
+	                production: true
+	            }
+	        },
+	        "dev": {
+	            options: {
+	                production: false
+	            }
+	        }
+	    },
+	    connect: {
+	    	server: {
+	    	      options: {
+	    	    	keepalive: true,
+	    	        port: 8080
+	    	      }
+	    	}	    	
+	    }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-
+	grunt.loadNpmTasks("grunt-bower-install-simple");
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	
 	// Default task(s).
 	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
-
+	grunt.registerTask('bower-install', ['bower-install-simple']);
+	grunt.registerTask('test', ['bower-install-simple', 'connect']);
 };
--- test/js/require-deps.js
+++ test/js/require-deps.js
@@ -3,8 +3,8 @@ require.config({
 
   },
   paths: {
-    jquery: "../bower_components/jquery/dist/jquery",
-    requirejs: "../bower_components/requirejs/require",
+    jquery: "../../bower_components/jquery/dist/jquery",
+    requirejs: "../../bower_components/requirejs/require",
     "saml-ecp-js": "../dist/saml-ecp-js"
   },
   packages: [
