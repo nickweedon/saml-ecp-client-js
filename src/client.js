7738734d2dcf2a933e4ccb2b2a835271ca24e142
Nick Weedon n2798271 <nick@weedon.org.au>
2015-02-25 09:45:47 +1000

    Further work on bower/grunt/amd scaffolding stuff
    - Added jquery.soap
    - Made samlEcpJs.client a class

--- src/client.js
+++ src/client.js
@@ -1,8 +1,42 @@
+
+
 var samlEcpJs = samlEcpJs || {};
 
-samlEcpJs.client = {
-		
-    doStuff : function(){
-        console.log('Yay! now working!');
-    }
+samlEcpJs.client = function(){
+ 
+	this.msg = 'Yay! it is now working!';
+	
+    this.doStuff = function() {
+
+    	
+    	/*
+    	$.soap({
+    	    url: 'http://www.google.com/soapservices/',
+    	    method: 'helloWorld',
+
+    	    data: {
+    	        name: 'Remy Blom',
+    	        msg: 'Hi!'
+    	    },
+
+    	    success: function (soapResponse) {
+    	    	
+    	    	console.debug("Yay!");
+    	        // do stuff with soapResponse
+    	        // if you want to have the response as JSON use soapResponse.toJSON();
+    	        // or soapResponse.toString() to get XML string
+    	        // or soapResponse.toXML() to get XML DOM
+    	    },
+    	    error: function (SOAPResponse) {
+    	    	
+    	    	console.debug("Error!");
+    	    	
+    	        // show error
+    	    }
+    	});
+    	*/
+    	
+    	
+    	console.log(this.msg);
+    };
 }; 
--- test/js/require-deps.js
+++ test/js/require-deps.js
@@ -5,7 +5,8 @@ require.config({
   paths: {
     jquery: "../../bower_components/jquery/dist/jquery",
     requirejs: "../../bower_components/requirejs/require",
-    "saml-ecp-js": "../dist/saml-ecp-js"
+    "saml-ecp-js": "../dist/saml-ecp-js",
+    "jquery.soap": "../../bower_components/jquery.soap/jquery.soap"
   },
   packages: [
 
