57e0b2b07a5a015148048632a7a13410cfb0e08e
Nick Weedon n2798271 <nick@weedon.org.au>
2015-02-24 23:10:36 +1000

    Added compile step to test target

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,5 +58,5 @@ module.exports = function(grunt) {
 	// Default task(s).
 	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 	grunt.registerTask('bower-install', ['bower-install-simple']);
-	grunt.registerTask('test', ['bower-install-simple', 'connect']);
+	grunt.registerTask('test', ['default', 'bower-install-simple', 'connect']);
 };
